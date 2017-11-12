9504ab88de515f9e44578008ae3592b74b844aaf
Muxxs <747306970@qq.com>
2017-11-13 00:26:08 +0800

    Update kolia_text.py

--- plu_for_service/kolia_text.py
+++ plu_for_service/kolia_text.py
@@ -1,7 +1,9 @@
 #coding=utf-8
-import threading, time, random
+import exercise
+from pub import get_config
+
+
 def play_mp3(filename):
-    import time
     import pygame
     file = filename
     pygame.mixer.init()
@@ -9,23 +11,28 @@ def play_mp3(filename):
     track = pygame.mixer.music.load(file)
     pygame.mixer.music.play()
 def say(word):
-
-    play_mp3("say.mp3")
-def try_understand(s):
-    from bosonnlp import BosonNLP
-    # 注意：在测试时请更换为您的API token。
-    nlp = BosonNLP('nPom9h4a.18434.tEA4SsUlkG8g')
     import os
-    feel=nlp.sentiment(s)
-    result = nlp.depparser(s)
-
-    print(' '.join(result[0]['word']))
-    print(' '.join(result[0]['tag']))
-    print(result[0]['head'])
-    print(' '.join(result[0]['role']))
-    text=""
-    threading.Thread(target=say, args=(text), name='thread-' + str(i)).start()
+    return os.system("say "+word)
 def translate(res):
     from bosonnlp import BosonNLP
     nlp = BosonNLP("nPom9h4a.18434.tEA4SsUlkG8g")
-    print nlp.sentiment(res)[0]
+    return nlp.sentiment(res)[0]
+def wake(words):
+    key_words= get_config.get_wake()
+    if words.find(key_words)<>-1:
+        return 1
+def text(content):
+    Main_word="贾维斯"
+    if content[:3]== Main_word or content.find(Main_word) <> -1:
+        global model
+        model=1
+        exercise.cut_word(content)
+    else:
+        feel=translate(content)[0]
+        global The_feel
+        if int(feel) >=0.5 :
+            The_feel=True
+        else:
+            The_feel=False
+        say_word=exercise.cut_word(content)
+        return say_word
