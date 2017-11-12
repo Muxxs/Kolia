0e5e55d277a0c9ebec37b113c29db690b568308d
Muxxs <747306970@qq.com>
2017-11-13 00:26:46 +0800

    Update sendMessage.py

--- plu/sendMessage.py
+++ plu/sendMessage.py
@@ -1,11 +1,12 @@
 #coding=utf-8
-
 def sendMessage(message,mac_id):
+    from pub import get_config
     import socket
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    sock.connect(('localhost', 8081))
+    sock.connect(('localhost', int(get_config.get_porttext())))
     from plu import word2
     message=word2.wordstotwo(message+'?/!'+mac_id)
-    sock.send(message)
-    print sock.recv(1024)
+    print sock.send(message)
+    recv= sock.recv(1024)
     sock.close()
+    return recv
